c961110eb13fe4dac2a983611064f5ec46458096
toxin <yuri.belousov.1998@gmail.com>
2018-04-06 18:27:13 +0300

    clear_

--- SPRING_DATA_REST/springDataRest/src/main/java/com/toxin/springdatarest/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.toxin.springdatarest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByName(String name);
-}
