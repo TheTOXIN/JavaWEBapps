a2ed83cd68174a3e5895f0ea3200328b8ff3a044
TheTOXIN <thetoksin@gmail.com>
2018-06-11 15:04:40 +0300

    Add template client

--- FULL_STACK/ToDoServer/src/main/java/com/toxin/todo/entity/User.java
+++ FULL_STACK/ToDoServer/src/main/java/com/toxin/todo/entity/User.java
@@ -1,5 +1,6 @@
 package com.toxin.todo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -21,6 +22,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String login;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 
